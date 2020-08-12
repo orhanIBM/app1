f7a3f9244a510c6b6755f410a2c9f19eb3124d02
Orhan-Yarar <orhan.yarar@ibm.com>
2020-08-12 19:20:58 -0400

    initial

---
+++ index.js
@@ -0,0 +1,16 @@
+
+const express = require('express')
+const app = express()
+const port = 8081
+const axios = require ("axios")
+
+app.get('/live', (req, res) => {
+  res.send('Alive!')
+})
+
+app.get("/connect", (req, res)=> {
+axios.get("/myservice/response").then(response => res.send(response)).catch(err => {console.log(err), res.send(JSON.stringify(error))})
+})
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
